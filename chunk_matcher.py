9ae5ad3912d41f27c8a673d899fb523d51a8e471
bartek <bartek@bartek-K53SV.(none)>
2013-08-13 19:57:43 +0200

    chunk matcher script

---
+++ chunk_matcher.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+
+import sys, re
+from optparse import OptionParser
+import itertools, sets 
+
+parser = OptionParser()
+(_, args) = parser.parse_args()
+
+
+if (len(args) < 2):
+    print("at least two label files are required")
+    sys.exit(1)
+
+
+def prepare_file(path):
+    lines = file(path).read().decode("utf8").lower().split("\n")[:-1]
+
+    tokenized = [line.split(" ") for line in lines]
+    converted = [(float(split[0]), float(split[1]), " ".join(split[2:])) for split in tokenized]
+    return [(start, end, re.split("[ .]", label)) for (start, end, label) in converted]
+    
+first = prepare_file(args[0])
+second = prepare_file(args[1])
+
+
+def merge_to_look_similar_by_times(merging, target):
+    pass
+
+def mergeLabels(labels):
+    if (not labels):
+        return (0, 0, "")
+    return reduce(
+        lambda (accStart, accEnd, accLabel), (start, end, label) :
+            (min(accStart, start), max(accEnd, end), accLabel + label),
+            labels)
+
+
+def merge_by_words(merging, target):
+    ret = []
+    for (start, end, label) in target:
+        reducing = label
+        toMerge = []
+        while (reducing and merging):
+            (currStart, currEnd, currLabel) = merging[0]
+            #print ("t: ", (reducing[:len(currLabel)], currLabel), (reducing[:len(currLabel)] == currLabel))
+            if (reducing[:len(currLabel)] != currLabel):
+                break
+            toMerge.append((currStart, currEnd, currLabel))
+            reducing=reducing[len(currLabel):]
+            merging = merging[1:]
+        #print(label, toMerge)
+        if (toMerge):
+            ret.append(mergeLabels(toMerge))
+        else:
+            break
+    return ret
+    
+def merge_by_times(merging, target):
+    def timeFilter((start, end, l), (tstart, tend, tl)):
+        return (start >= tstart - 0.1) and (end <= tend - 0.1)
+    def extractFromTime(target):
+        return mergeLabels([label for label in merging if timeFilter(label, target)])
+    return [extractFromTime(label) for label in target]
+    
+
+def countTimeDifferent(first, second, diff):
+    if (len(first) != len(second)):
+        print "error: first and second length don't agree"
+        sys.exit(1)
+
+    total = 0
+    aveStartDiff = 0.0
+    aveEndDiff = 0.0
+    maxDiff = 0.0
+    for ((start1, end1, label1), (start2, end2, label2)) in zip(first, second):
+        if ("".join(label1) != "".join(label2)):
+            print "error: zipped labels don't agree ", label1, label2
+            sys.exit(1)
+        startDiff = abs(start2 - start1)
+        endDiff = abs(end2 - end1)
+        aveStartDiff += startDiff
+        aveEndDiff += endDiff
+        maxDiff = max(maxDiff, startDiff, endDiff)
+        if (startDiff > diff) or (endDiff > diff):
+            total += 1
+    return (len(second) - total, len(second), aveStartDiff / len(first), aveEndDiff / len(first), maxDiff)
+     
+def countWordDifferent(first, second):
+    def wordDiff(first, second):
+        matched = 0
+        for i in range(0, len(first) + 1):
+            for j in range(0, len(second) + 1):
+                for k in range(i + 1, len(first) + 1):
+                    if (first[i:k] == second[j:(j + k - i)]):
+                        matched = max(matched, k - i)
+        if (len(args) > 3) and (matched == 0):
+            print (first, second)
+        return len(first) + len(second) - 2 * matched
+
+    diffs = [wordDiff(label1, label2) for ((s1, e1, label1), (s2, e2, label2)) in zip(first, second)]
+    ret = {}
+    for diff in diffs:
+        if (ret.has_key(diff)):
+            ret[diff] += 1
+        else:
+            ret[diff] = 1
+    return ret
+
+firstMergedByWords = merge_by_words(first, second)       
+firstMergedByTimes = merge_by_times(first, second)       
+    
+print countTimeDifferent(firstMergedByWords, second, 0.5)
+print countWordDifferent(firstMergedByTimes, second)
+
+
