542aa1fe04ac6bd9acd3b1be11d01c890e5e4903
bartek <bartek@bartek-K53SV.(none)>
2013-08-13 22:16:45 +0200

    incremental aligner fixes

--- src/textAligners/IncrementalTextToSpeechAligner.java
+++ src/textAligners/IncrementalTextToSpeechAligner.java
@@ -1,11 +1,13 @@
 package textAligners;
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 import common.AudioLabel;
 import common.ITextProducer;
 import common.Speeches;
 import common.Text;
+import common.exceptions.ImplementationError;
 
 
 
@@ -20,25 +22,36 @@ public class IncrementalTextToSpeechAligner implements ITextToSpeechAligner {
 	
 	class ReducableTextProducer implements ITextProducer
 	{
-		private ArrayList<String> sentences = null;
+		private ArrayList<String> sentences = new ArrayList<String>();
 		
 		public ReducableTextProducer(String[] sentences)
 		{
-			this.sentences = new ArrayList<String>();
 			for (String sentence : sentences) this.sentences.add(sentence);
 		}
 		
 		void removeAllFromFront(String text)
 		{
-			while (!sentences.isEmpty() && (text.indexOf(sentences.get(0)) >= 0)) {
-				sentences.remove(0);
-			}
+		    while (!this.sentences.isEmpty() && !text.isEmpty()) {
+		        String current = this.sentences.get(0);
+		        int index = text.indexOf(current);
+		        if (index != 0)
+		            throw new ImplementationError("something missing: ->" + text + "<- " + current);
+		        this.sentences.remove(0);
+		        if (text.length() == current.length()) break;
+		        text = text.substring(current.length() + 1);
+		    }
 		}
 		void removeAllFromBack(String text)
 		{
-			while (!sentences.isEmpty() && (text.indexOf(sentences.get(sentences.size() - 1)) >= 0)) {
-				sentences.remove(sentences.size() - 1);
-			}
+            while (!this.sentences.isEmpty() && !text.isEmpty()) {
+                String current = this.sentences.get(this.size() - 1);
+                int index = text.indexOf(current, text.length() - current.length());
+                if (index + current.length() != text.length())
+                    throw new ImplementationError("something missing: ->" + text + "<- " + current);
+                this.sentences.remove(this.size() - 1);
+                if (text.length() == current.length()) break;
+                text = text.substring(0, index - 1);
+            }
 		}
 		int size() {
 			return sentences.size();
@@ -70,36 +83,33 @@ public class IncrementalTextToSpeechAligner implements ITextToSpeechAligner {
 			System.err.println("left for alignment: " + speeches.size() + " " + textProducer.size());
 			AudioLabel[] curr = aligner.findMatching(new Text(textProducer, speeches.getTotalTime()), speeches);
 			textProducer.removeAllFromFront(curr[0].getLabel());
-			removeAllFromFront(speeches, curr[0].getEnd() - curr[0].getStart());
+			removeAllFromFront(speeches, curr[0].getEnd());
 			textProducer.removeAllFromBack(curr[curr.length - 1].getLabel());
-			removeAllFromBack(speeches, curr[curr.length - 1].getEnd() - curr[curr.length - 1].getStart());
+			removeAllFromBack(speeches, curr[curr.length - 1].getStart());
 			front.add(curr[0]);
 			back.add(curr[curr.length - 1]);
 		}
 		
+		Collections.reverse(back);
 		for (AudioLabel label : back) front.add(label);
 		return front.toArray(new AudioLabel[0]);
 	}
 
 	private void removeAllFromBack(Speeches speeches, double time)
 	{
-		while ((time > 0.1) && (speeches.size() > 0)) {
-			double speechTime = speeches.get(speeches.size() - 1).getTime();
-			if (time - speechTime > -0.1) {
-				time -= speechTime;
-				speeches.pop_back();
-			} else break;
+		while (speeches.size() > 0) {
+		    if (speeches.get(speeches.size() - 1).getStartTime() + 0.1 >= time)
+    			speeches.pop_back();
+			else break;
 		}
 	}
 
 	private void removeAllFromFront(Speeches speeches, double time)
 	{
-		while ((time > 0.1) && (speeches.size() > 0)) {
-			double speechTime = speeches.get(0).getTime();
-			if (time - speechTime > -0.1) {
-				time -= speechTime;
-				speeches.pop_front();
-			} else break;
-		}
+        while (speeches.size() > 0) {
+            if (speeches.get(0).getEndTime() - 0.1 <= time)
+                speeches.pop_front();
+            else break;
+        }
 	}
 }
