c58d9006722236bbbac6366e858acb6bf18feba2
bartek <bartek@bartek-K53SV.(none)>
2013-08-16 16:24:09 +0200

    added phoneme queue with max size, but per index in aligning sequence

--- src/textAligners/BreadthSearchBestCutPhonemeAligner.java
+++ src/textAligners/BreadthSearchBestCutPhonemeAligner.java
@@ -3,6 +3,7 @@ package textAligners;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -43,29 +44,121 @@ public class BreadthSearchBestCutPhonemeAligner
         double frameTime = totalTime / audio.size();
         
 
-        SortedSet<CuttingScorer> scorers = new TreeSet<CuttingScorer>(scorerComparator);
-        scorers.add(new CuttingScorer(
+        ScorerQueue queue = new ScorerQueue(numOfScorers);
+        queue.add(new CuttingScorer(
                 this.phonemeScorers.get(phonemes[0]), 0, null, 0, 0));
         
         System.err.println("audio size: " + audio.size() + " phonemes length: " + phonemes.length);
         for (int i = 0; i < audio.size(); ++i) {
-            SortedSet<CuttingScorer> nextScorers = new TreeSet<CuttingScorer>(scorerComparator);
+            ScorerQueue nextQueue = new ScorerQueue(numOfScorers);
             
-            for (CuttingScorer scorer : scorers) {
+            for (CuttingScorer scorer : queue) {
                 IPhonemeScorer nextScorer = (scorer.getIndex() + 1 >= phonemes.length) ? null :
                     this.phonemeScorers.get(phonemes[scorer.getIndex() + 1]);
-                nextScorers.addAll(scorer.score(audio.get(i), i * frameTime, nextScorer));
+                for (CuttingScorer nextCScorer : scorer.score(audio.get(i), i * frameTime, nextScorer))
+                    nextQueue.add(nextCScorer);
             }
             
-            while (nextScorers.size() > numOfScorers) {
-                nextScorers.remove(nextScorers.last());
-            }
-            scorers = nextScorers;
+            queue = nextQueue;
             if ((i + 1) % 1000 == 0)
-                System.err.println(i + " "  + scorers.first().getScore() + " " + scorers.last().getScore());
+                System.err.println(i + " "  + queue.first().getScore() + " " + queue.last().getScore());
+            if (i > 10000)
+                break;
         }
         
-        return scorers.first().getBestAlignment(totalTime);
+        return queue.first().getBestAlignment(totalTime);
+    }
+    
+    private class ScorerQueue implements Iterable<CuttingScorer>
+    {
+        private final int maxSize;
+        private Map<Integer, CuttingScorer> scorersPerIndex = new HashMap<Integer, CuttingScorer>();
+        
+        private class ScoringIndex
+        {
+            final int index;
+            final double score;
+            public ScoringIndex(double score, int index)
+            {
+                this.index = index;
+                this.score = score;
+            }
+        }
+        private final Comparator<ScoringIndex> scorerComparator =
+                new Comparator<ScoringIndex>() {
+                    @Override
+                    public int compare(ScoringIndex arg0, ScoringIndex arg1)
+                    {
+                        if (arg0.score < arg1.score) return 1;
+                        if (arg0.score > arg1.score) return -1;
+                        return 0;
+                    }
+                };
+        
+        private SortedSet<ScoringIndex> bestScoringIndexes = new TreeSet<ScoringIndex>(scorerComparator);
+        
+        public ScorerQueue(int maxSize)
+        {
+            this.maxSize = maxSize;
+        }
+        public CuttingScorer first()
+        {
+            return this.scorersPerIndex.get(this.bestScoringIndexes.first().index);
+        }
+        public CuttingScorer last()
+        {
+            return this.scorersPerIndex.get(this.bestScoringIndexes.last().index);
+        }
+        public void add(CuttingScorer scorer) {
+            if (this.scorersPerIndex.containsKey(scorer.getIndex())) {
+                double currentScore = this.scorersPerIndex.get(scorer.getIndex()).getScore();
+                if (currentScore < scorer.getScore()) {
+                    int previousSize = this.bestScoringIndexes.size();
+                    this.bestScoringIndexes.remove(new ScoringIndex(currentScore, scorer.getIndex()));
+                    if (this.bestScoringIndexes.size() + 1 != previousSize)
+                        throw new ImplementationError("not removed from best scoring indexes");
+                    this.bestScoringIndexes.add(new ScoringIndex(scorer.getScore(), scorer.getIndex()));
+                    if (this.bestScoringIndexes.size() != previousSize)
+                        throw new ImplementationError("not added to best scoring indexes");
+                    this.scorersPerIndex.put(scorer.getIndex(), scorer);
+                }
+            } else if (this.bestScoringIndexes.size() < this.maxSize) {
+                this.bestScoringIndexes.add(new ScoringIndex(scorer.getScore(), scorer.getIndex()));
+                this.scorersPerIndex.put(scorer.getIndex(), scorer);
+            } else if (this.bestScoringIndexes.last().score < scorer.getScore()) {
+                this.bestScoringIndexes.add(new ScoringIndex(scorer.getScore(), scorer.getIndex()));
+                this.scorersPerIndex.put(scorer.getIndex(), scorer);
+                
+                this.scorersPerIndex.remove(this.bestScoringIndexes.last().index);
+                this.bestScoringIndexes.remove(this.bestScoringIndexes.last());
+            }
+        }
+        @Override
+        public Iterator<CuttingScorer> iterator()
+        {
+            return new Iterator<CuttingScorer>() {
+                private Iterator<ScoringIndex> indexIterator = bestScoringIndexes.iterator();
+                @Override
+                public boolean hasNext()
+                {
+                    return indexIterator.hasNext();
+                }
+                @Override
+                public CuttingScorer next()
+                {
+                    ScoringIndex nextIndex = indexIterator.next();
+                    if (nextIndex == null) return null;
+                    if (!scorersPerIndex.containsKey(nextIndex.index))
+                        throw new ImplementationError("no such index in scorers per index");
+                    return scorersPerIndex.get(nextIndex.index);
+                }
+                @Override
+                public void remove()
+                {
+                    throw new ImplementationError("remove unimplemented");
+                }
+            };
+        }
     }
     
     private class CuttingScorer
