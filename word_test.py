0199ecc14608286cbffcd29e832886599b48978b
bartek <bartek@bartek-K53SV.(none)>
2013-08-18 20:04:36 +0200

    word tests - russian model

--- word_test.py
+++ word_test.py
@@ -38,30 +38,28 @@ def createStats(checked, target, errorThresholds):
         if (e1 < e2):
             maxShortDiff = max(endDiff, maxShortDiff)
         aveDiff += endDiff + startDiff
-        aveStartDiff += startDiff
-        aveEndDiff += endDiff
         for threshold in errorThresholds:
+            assignedToBothAlready = False
             if (startDiff > threshold):
                 startCounts[threshold] += 1
                 if (s1 > s2):
                     startIfShortCounts[threshold] += 1
+                    bothIfShortCounts[threshold] += 1
+                    assignedToBothAlready = True
             if (endDiff > threshold):
                 endCounts[threshold] += 1
                 if (e1 < e2):
                     endIfShortCounts[threshold] += 1
+                    if (not assignedToBothAlready):
+                        bothIfShortCounts[threshold] += 1
             if (startDiff > threshold) or (endDiff > threshold):
                 bothCounts[threshold] += 1
-                if (s1 > s2) or (e1 < e2):
-                    bothIfShortCounts[threshold] += 1
     aveDiff /= 2 * len(checked)
-    aveStartDiff /= len(checked)
-    aveEndDiff /= len(checked)
 
     return (len(checked),
             maxDiff,
+            maxShortDiff,
             aveDiff,
-            aveStartDiff,
-            aveEndDiff,
             startCounts,
             startIfShortCounts,
             endCounts,
@@ -83,4 +81,4 @@ target = prepare_file(args[1])
 print createStats(checked, target, [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 1])
 
 # russian model word alignment
-#(585, 1.3542329999999936, 0.03297232435897484, 0.03197732649572708, 0.03396732222222261, {0.5: 9, 1: 2, 0.4: 10, 0.2: 19, 0.05: 66, 0.3: 15, 0.1: 34}, {0.5: 0, 1: 0, 0.4: 0, 0.2: 0, 0.05: 0, 0.3: 0, 0.1: 0}, {0.5: 8, 1: 2, 0.4: 11, 0.2: 26, 0.05: 78, 0.3: 18, 0.1: 42}, {0.5: 1, 1: 0, 0.4: 1, 0.2: 1, 0.05: 21, 0.3: 1, 0.1: 5}, {0.5: 16, 1: 4, 0.4: 20, 0.2: 44, 0.05: 132, 0.3: 32, 0.1: 73}, {0.5: 4, 1: 1, 0.4: 4, 0.2: 5, 0.05: 28, 0.3: 5, 0.1: 10})
+#(585, 1.3542329999999936, 0.5336800000000039, 0.03297232435897484, 0.03197732649572708, 0.03396732222222261, {0.5: 9, 1: 2, 0.4: 10, 0.2: 19, 0.05: 66, 0.3: 15, 0.1: 34}, {0.5: 0, 1: 0, 0.4: 0, 0.2: 0, 0.05: 0, 0.3: 0, 0.1: 0}, {0.5: 8, 1: 2, 0.4: 11, 0.2: 26, 0.05: 78, 0.3: 18, 0.1: 42}, {0.5: 1, 1: 0, 0.4: 1, 0.2: 1, 0.05: 21, 0.3: 1, 0.1: 5}, {0.5: 16, 1: 4, 0.4: 20, 0.2: 44, 0.05: 132, 0.3: 32, 0.1: 73}, {0.5: 4, 1: 1, 0.4: 4, 0.2: 5, 0.05: 28, 0.3: 5, 0.1: 10})
